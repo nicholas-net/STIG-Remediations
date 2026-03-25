961ca6801ebae848b09f957c31cec1cc6807b94c
Nicholas <cloudvibeclothing@gmail.com>
2026-03-25 12:18:26 -0400

    updated dates

--- STIGS/WN11-CC-000315.ps1
+++ STIGS/WN11-CC-000315.ps1
@@ -6,15 +6,15 @@
     Author          : Nicholas Colon
     LinkedIn        : linkedin.com/in/nick-colon
     GitHub          : github.com/nicholas-net
-    Date Created    : 2026-03-20
-    Last Modified   : 2026-03-20
+    Date Created    : 2026-03-25
+    Last Modified   : 2026-03-25
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN11-CC-000315 
 
 .TESTED ON
-    Date(s) Tested  : 2026-03-20
+    Date(s) Tested  : 2026-03-25
     Tested By       : Nicholas Colon
     Systems Tested  : Windows 11 Pro (Azure VM) - Build 26200
     PowerShell Ver. : 5.1
