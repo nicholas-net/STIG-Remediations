ec96737bf467119ea6a74f41032e2e6901055c57
Nicholas <cloudvibeclothing@gmail.com>
2026-03-27 12:26:05 -0400

    Update WN11-CC-000260.ps1

--- STIGS/WN11-CC-000260.ps1
+++ STIGS/WN11-CC-000260.ps1
@@ -8,15 +8,15 @@
     Author          : Nicholas Colon
     LinkedIn        : linkedin.com/in/nick-colon
     GitHub          : github.com/nicholas-net
-    Date Created    : 2026-03-26
-    Last Modified   : 2026-03-26
+    Date Created    : 2026-03-27
+    Last Modified   : 2026-03-27
     Version         : 1.0
     CVEs            : N/A
     Plugin IDs      : N/A
     STIG-ID         : WN11-CC-000260
 
 .TESTED ON
-    Date(s) Tested  : 2026-03-26
+    Date(s) Tested  : 2026-03-27
     Tested By       : Nicholas Colon
     Systems Tested  : Windows 11 Pro (Azure VM) - Build 26200
     PowerShell Ver. : 5.1
